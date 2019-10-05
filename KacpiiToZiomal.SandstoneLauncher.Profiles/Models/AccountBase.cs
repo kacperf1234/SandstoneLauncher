d276697cb0528f589eabe9ad274ce7a80a677608
Kacper Faber <kacperf1234@gmail.com>
2019-10-05 16:02:19 +0200

    Added models for Accounts (premium, nonpremium and base)

--- KacpiiToZiomal.SandstoneLauncher.Profiles/Models/AccountBase.cs
+++ KacpiiToZiomal.SandstoneLauncher.Profiles/Models/AccountBase.cs
@@ -1,7 +1,16 @@
-﻿namespace KacpiiToZiomal.SandstoneLauncher.Profiles.Models
+﻿using Newtonsoft.Json;
+
+namespace KacpiiToZiomal.SandstoneLauncher.Profiles.Models
 {
-    public class AccountBase
+    public abstract class AccountBase
     {
+        [JsonProperty("username")]
+        public string GameUsername { get; set; }
         
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("uuid")]
+        public string UUID { get; set; }
     }
 }

5546a82699d0e8f15339e0ec7dd34cc2d9b69914
Kacper Faber <kacperf1234@gmail.com>
2019-10-05 12:31:46 +0200

    in SandstoneLauncher project added ApplicationData, IApplicationData. It works like IMinecraftDirectory and implementation in minecraft. And now we have tests for one method from Appdata.

---
+++ KacpiiToZiomal.SandstoneLauncher.Profiles/Models/Profile.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace KacpiiToZiomal.SandstoneLauncher.Profiles.Models
+{
+    public class Profile
+    {
+        [JsonProperty("profile_name")]
+        public string ProfileName { get; set; }
+        
+        [JsonProperty("profile_version")]
+        public string ProfileVersion { get; set; }
+    }
+}

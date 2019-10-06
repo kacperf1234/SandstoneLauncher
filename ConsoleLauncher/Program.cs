037e1bb5ec89c4d7058b3fa9690f66d8d61ac524
Kacper Faber <kacperf1234@gmail.com>
2019-10-06 13:55:38 +0200

    is there something in xaml. Core of the Profiles project are now can be have profiles from file and load it to window as model Jebac Xamla

---
+++ ConsoleLauncher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using KacpiiToZiomal.SandstoneLauncher.Minecraft.Types;
+using KacpiiToZiomal.SandstoneLauncher.Profiles.Models;
+using KacpiiToZiomal.SandstoneLauncher.Profiles.Types;
+using KacpiiToZiomal.SandstoneLauncher.Types;
+
+namespace ConsoleLauncher
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            ProfileGetter getter = new ProfileGetter();
+            getter.ProfilesReader = new ProfilesReader(new ProfilesPathGenerator(new ApplicationData()), new FileReader(), new ProfilesDeserializer());
+            Profile profile = null;
+
+            while (true)
+            {
+                profile = getter.GetProfile(new ProfileGetterSettings());
+                Console.WriteLine(profile.ProfileName);
+            }
+                
+        }
+    }
+}

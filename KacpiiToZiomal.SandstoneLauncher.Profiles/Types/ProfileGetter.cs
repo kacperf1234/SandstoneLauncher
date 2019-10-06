28c3c6faf8d51cf30fd16fdb3d17095f33e210f7
Kacper Faber <kacperf1234@gmail.com>
2019-10-06 13:55:38 +0200

    is there something in xaml. Core of the Profiles project are now can be have profiles from file and load it to window as model Jebac Xamla

--- KacpiiToZiomal.SandstoneLauncher.Profiles/Types/ProfileGetter.cs
+++ KacpiiToZiomal.SandstoneLauncher.Profiles/Types/ProfileGetter.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using KacpiiToZiomal.SandstoneLauncher.Profiles.Interfaces;
 using KacpiiToZiomal.SandstoneLauncher.Profiles.Models;
+using KacpiiToZiomal.SandstoneLauncher.Profiles.Windows;
 
 namespace KacpiiToZiomal.SandstoneLauncher.Profiles.Types
 {
     public class ProfileGetter : IProfileGetter
     {
+        public IProfilesReader ProfilesReader;
+        
         public Profile GetProfile(ProfileGetterSettings settings)
         {
             // get all data about existing profiles...
-            Models.ProfileCollection profileCollection = null;
+            ProfileCollection profileCollection = ProfilesReader.ReadProfiles();
             
             // use received data as model in window.
-            
+            ProfilesWindow profilesWnd = new ProfilesWindow(profileCollection);
+            profilesWnd.ShowDialog();
             
             
             throw new NotImplementedException();
---
+++ KacpiiToZiomal.SandstoneLauncher.Profiles/Windows/ProfilesWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using KacpiiToZiomal.SandstoneLauncher.Profiles.Models;
+
+namespace KacpiiToZiomal.SandstoneLauncher.Profiles.Windows
+{
+    public partial class ProfilesWindow : Window
+    {
+        public List<Profile> Profiles { get; set; }
+        
+        public ProfilesWindow(ProfileCollection profiles)
+        {
+            Profiles = profiles.Profiles;
+
+            DataContext = this;
+            InitializeComponent();
+        }
+    }
+}

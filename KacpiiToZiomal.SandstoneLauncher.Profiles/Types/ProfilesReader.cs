2c94dccf08da72a3430a7ec5ca612fe059b42a9f
Kacper Faber <kacperf1234@gmail.com>
2019-10-06 12:50:02 +0200

    Added profilesreader and writer.

---
+++ KacpiiToZiomal.SandstoneLauncher.Profiles/Types/ProfilesReader.cs
@@ -0,0 +1,21 @@
+﻿using KacpiiToZiomal.SandstoneLauncher.Minecraft.Interfaces;
+using KacpiiToZiomal.SandstoneLauncher.Profiles.Interfaces;
+using KacpiiToZiomal.SandstoneLauncher.Profiles.Models;
+
+namespace KacpiiToZiomal.SandstoneLauncher.Profiles.Types
+{
+    public class ProfilesReader : IProfilesReader
+    {
+        public IProfilesPathGenerator ProfilesPathGenerator;
+        public IFileReader Reader;
+        public IProfilesDeserializer Deserializer;
+        
+        public ProfileCollection ReadProfiles()
+        {
+            string path = ProfilesPathGenerator.GeneratePath();
+            string content = Reader.Read(path);
+
+            return Deserializer.Deserialize(content);
+        }
+    }
+}

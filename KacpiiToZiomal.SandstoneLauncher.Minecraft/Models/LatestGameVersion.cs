c11676a1734ff0f6e893502943fa62c131c131e1
Kacper Faber <kacperf1234@gmail.com>
2019-11-09 11:07:26 +0100

    Removed slndirectories. Added method and private ctor for GameVersion

--- KacpiiToZiomal.SandstoneLauncher.Minecraft/Models/LatestGameVersion.cs
+++ KacpiiToZiomal.SandstoneLauncher.Minecraft/Models/LatestGameVersion.cs
@@ -4,8 +4,10 @@ namespace KacpiiToZiomal.SandstoneLauncher.Minecraft.Models
 {
     public class LatestGameVersion
     {
-        [JsonProperty("release")] public string Release { get; set; }
+        [JsonProperty("release")] 
+        public string Release { get; set; }
 
-        [JsonProperty("snapshot")] public string Snapshot { get; set; }
+        [JsonProperty("snapshot")]
+        public string Snapshot { get; set; }
     }
 }

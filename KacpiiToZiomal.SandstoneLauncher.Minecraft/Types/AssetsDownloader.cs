27961675f0cc988ac1d9d82c10406a63ddbf06eb
Kacper Faber <kacperf1234@gmail.com>
2019-11-09 15:59:57 +0100

    if i using downloadasync in assetsdownloader, only creating index and loop or idk

--- KacpiiToZiomal.SandstoneLauncher.Minecraft/Types/AssetsDownloader.cs
+++ KacpiiToZiomal.SandstoneLauncher.Minecraft/Types/AssetsDownloader.cs
@@ -26,17 +26,13 @@ namespace KacpiiToZiomal.SandstoneLauncher.Minecraft.Types
 
         public void Download(Assets assets, FullVersion version)
         {
-            Console.WriteLine(assets.Keys.Count());
-            
             foreach (string key in assets.AssetList.Keys)
             {
                 Asset asset = Extractor.Get(assets, key);
                 string url = UrlBuilder.BuildUrl(asset.Hash);
                 string dest = PathBuilder.GetAbsolutePath(asset.Hash);
 
-                Console.WriteLine($"hash: {asset.Hash}\ntrace: {url} --> {dest}\n\n");
-
-                Downloader.DownloadAsync(url, dest);
+                Downloader.Download(url, dest);
             }
 
             IndexCreator.Create(assets.BaseJson, version.Assets);

6eb3be80f9a40ab6b0383b421c357290c38ec396
Kacper Faber <kacperf1234@gmail.com>
2019-11-15 16:03:42 +0100

    Added project Accounts

--- KacpiiToZiomal.SandstoneLauncher.Accounts/obj/Debug/netcoreapp3.0/KacpiiToZiomal.SandstoneLauncher.Accounts.AssemblyInfo.cs
+++ KacpiiToZiomal.SandstoneLauncher.Accounts/obj/Debug/netcoreapp3.0/KacpiiToZiomal.SandstoneLauncher.Accounts.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("KacpiiToZiomal.SandstoneLauncher.CommonControls")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("KacpiiToZiomal.SandstoneLauncher.Accounts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("KacpiiToZiomal.SandstoneLauncher.CommonControls")]
-[assembly: System.Reflection.AssemblyTitleAttribute("KacpiiToZiomal.SandstoneLauncher.CommonControls")]
+[assembly: System.Reflection.AssemblyProductAttribute("KacpiiToZiomal.SandstoneLauncher.Accounts")]
+[assembly: System.Reflection.AssemblyTitleAttribute("KacpiiToZiomal.SandstoneLauncher.Accounts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Wygenerowane przez klasę WriteCodeFragment programu MSBuild.

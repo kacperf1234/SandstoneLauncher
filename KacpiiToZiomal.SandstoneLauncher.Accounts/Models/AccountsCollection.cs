8f65fd37980a5f09a104626af79c0085ecec7396
Kacper Faber <kacperf1234@gmail.com>
2019-11-17 11:30:58 +0100

    added Profile and Account model

--- KacpiiToZiomal.SandstoneLauncher.Accounts/Models/AccountsCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace KacpiiToZiomal.SandstoneLauncher.Accounts.Models
-{
-    public class AccountsCollection : List<Account>
-    {
-        
-    }
-}
--- KacpiiToZiomal.SandstoneLauncher/Models/MainWindowModel.cs
+++ KacpiiToZiomal.SandstoneLauncher/Models/MainWindowModel.cs
@@ -1,9 +1,9 @@
-﻿using KacpiiToZiomal.SandstoneLauncher.Accounts.Models;
+﻿
 
 namespace KacpiiToZiomal.SandstoneLauncher.Models
 {
     public class MainWindowModel
     {
-        public Account LoggedAccount { get; set; }
+        
     }
 }

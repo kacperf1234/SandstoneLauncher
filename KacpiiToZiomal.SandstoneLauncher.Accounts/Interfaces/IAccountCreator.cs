60e7f150842b90c9a284254dc9cfec57f8896d46
Kacper Faber <kacperf1234@gmail.com>
2019-11-17 11:19:28 +0100

    Added profiles project and referenced to him a mc.commons project

---
+++ KacpiiToZiomal.SandstoneLauncher.Accounts/Interfaces/IAccountCreator.cs
@@ -0,0 +1,11 @@
+﻿using KacpiiToZiomal.SandstoneLauncher.Accounts.Models;
+
+namespace KacpiiToZiomal.SandstoneLauncher.Accounts.Interfaces
+{
+    public interface IAccountCreator
+    {
+        OnlineAccount CreateOnlineAccount();
+
+        OfflineAccount CreateOfflineAccount();
+    }
+}

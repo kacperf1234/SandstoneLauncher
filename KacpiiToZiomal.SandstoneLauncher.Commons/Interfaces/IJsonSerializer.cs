345b99ee6a4bfc238064796f2bca04a1db4889da
Kacper Faber <kacperf1234@gmail.com>
2019-11-22 17:07:32 +0100

    added languageservice

---
+++ KacpiiToZiomal.SandstoneLauncher.Commons/Interfaces/IJsonSerializer.cs
@@ -0,0 +1,7 @@
+﻿namespace KacpiiToZiomal.SandstoneLauncher.Commons.Interfaces
+{
+    public interface IJsonSerializer <T>
+    {
+        string Serialize(T arg);
+    }
+}
--- KacpiiToZiomal.SandstoneLauncher.Languages/Types/LanguageLoader.cs
+++ KacpiiToZiomal.SandstoneLauncher.Languages/Types/LanguageLoader.cs
@@ -28,9 +28,19 @@ namespace KacpiiToZiomal.SandstoneLauncher.Languages.Types
             LanguageLoader loader = new LanguageLoader(
                 new LanguagesProvider(new FileListGenerator(), new LanguageFilesFilter(new LanguageFileNameValidator()),
                     new JsonDeserializer<Language>(), new FileReader(), new ApplicationData()), new LanguageExtractor(),
-                new ResourceDictionaryGenerator(new ResourceKeyNameGenerator()), new ResourceDictionaryMerger(), new ActuallyLanguageProvider(new JsonDeserializer<ActuallyLanguage>(), new FileReader(), new ActuallyLanguagePathGenerator(new ApplicationData())));
+                new ResourceDictionaryGenerator(new ResourceKeyNameGenerator()), new ResourceDictionaryMerger(), new ActuallyLanguageProvider(new JsonDeserializer<Language>(), new FileReader(), new ActuallyLanguagePathGenerator(new ApplicationData())));
             
             loader.Load(baseResources, shortname);
+        }
+
+        public static void Load(Language language, ResourceDictionary baseResources)
+        {
+            LanguageLoader loader = new LanguageLoader(
+                new LanguagesProvider(new FileListGenerator(), new LanguageFilesFilter(new LanguageFileNameValidator()),
+                    new JsonDeserializer<Language>(), new FileReader(), new ApplicationData()), new LanguageExtractor(),
+                new ResourceDictionaryGenerator(new ResourceKeyNameGenerator()), new ResourceDictionaryMerger(), new ActuallyLanguageProvider(new JsonDeserializer<Language>(), new FileReader(), new ActuallyLanguagePathGenerator(new ApplicationData())));
+
+            loader.Load(baseResources, language);
         }
 
         public void Load(ResourceDictionary baseResources, string shortname)
@@ -42,11 +52,17 @@ namespace KacpiiToZiomal.SandstoneLauncher.Languages.Types
             Merger.Merge(resourceDictionary, ref baseResources);
         }
 
+        public void Load(ResourceDictionary baseResources, Language language)
+        {
+            ResourceDictionary resourceDictionary = Generator.GenerateResourceDictionary(language);
+            Merger.Merge(resourceDictionary, ref baseResources);
+        }
+
         public void LoadActually(ResourceDictionary baseResource)
         {
             m.Languages languages = Provider.ProvideLanguages();
-            ActuallyLanguage actuallyLanguage = ActuallyLanguageProvider.ProvideActuallyLanguage();
-            Language language = Extractor.GetLanguage(languages, actuallyLanguage.ShortName);
+            Language actuallyLanguage = ActuallyLanguageProvider.ProvideActuallyLanguage();
+            Language language = Extractor.GetLanguage(languages, actuallyLanguage.Data.LongName);
             ResourceDictionary resourceDictionary = Generator.GenerateResourceDictionary(language);
             Merger.Merge(resourceDictionary, ref baseResource);
         }

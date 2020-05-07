c90cb073da1e5d6b8acd56868dd549d2797e470b
Kacper Faber <kacperf1234@gmail.com>
2020-05-07 10:26:09 +0200

    Created netcore 3.1 app project.

---
+++ SandstoneLauncher.App/Startup.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElectronNET.API;
+using ElectronNET.API.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace SandstoneLauncher.App
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(x => x.MapControllers());
+
+            Task.Run(async () =>
+            {
+                BrowserWindowOptions options = new BrowserWindowOptions()
+                {
+                    Center = true,
+                    Width = 800,
+                    Height = 600
+                };
+
+                BrowserWindow window = await Electron.WindowManager.CreateWindowAsync(options);
+                window.OnReadyToShow += () => window.Show();
+            });
+        }
+    }
+}

d7bebc1d05f65b3e45beb165ceb3d75e5b9b747f
Kacper Faber <kacperf1234@gmail.com>
2023-07-25 17:24:20 +0200

    ProcessOutputReader
    Created implementation.

---
+++ SandstoneLauncher.Minecraft/Types/ProcessOutputReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SandstoneLauncher.Minecraft.Interfaces;
+using Spencer.NET;
+
+namespace SandstoneLauncher.Minecraft.Types
+{
+    [SingleInstance]
+    public class ProcessOutputReader : IProcessOutputReader
+    {
+        public void Read(string content)
+        {
+            Console.WriteLine($"OUT: {content}");
+        }
+    }
+}

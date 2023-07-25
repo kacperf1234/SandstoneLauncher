16d722de61ebee147ebdbce646661e808354b07d
Kacper Faber <kacperf1234@gmail.com>
2023-07-25 11:43:37 +0200

    OperationResponse
    It's the class to represent response model, where "endpoint" returns OperationResponse
    
    OperationResponse is
    +op: str
    +data: any.
    
    OperationResponse has static PrintNew(op, data) and Print(OperationResponse)

---
+++ SandstoneLauncher.Minecraft.Cli/Response/OperationResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SandstoneLauncher.Minecraft.Cli.Response
+{
+    public class OperationResponse
+    {
+        [JsonProperty("op")]
+        public string Operation { get; set; }
+        
+        [JsonProperty("data")]
+        public object Data { get; set; }
+
+        public static void PrintNew(string op, object data)
+        {
+            var r = new OperationResponse { Operation = op, Data = data };
+            Console.WriteLine(JsonConvert.SerializeObject(r));
+        }
+
+        public static void Print(OperationResponse r)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(r));
+        }
+    }
+}

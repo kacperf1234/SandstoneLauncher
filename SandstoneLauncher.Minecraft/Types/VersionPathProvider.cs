5ed5d233e4f3720f6a85ce6d7066f59dea4b57d5
Kacper Faber <kacperf1234@gmail.com>
2023-07-25 11:06:35 +0200

    .Minecraft
    Created VersionPathProvider.

---
+++ SandstoneLauncher.Minecraft/Types/VersionPathProvider.cs
@@ -0,0 +1,13 @@
+﻿using Spencer.NET;
+
+namespace SandstoneLauncher.Minecraft.Types
+{
+    [SingleInstance]
+    public class VersionPathProvider
+    {
+        public string ProvideVersionPath(string versionName)
+        {
+            return new MinecraftDirectory().GetVersions() + versionName;
+        }
+    }
+}

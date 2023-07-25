5495077eeca576e314fd58f997801d28c9c96de0
Kacper Faber <kacperf1234@gmail.com>
2023-07-25 11:45:55 +0200

    Created InstalledVersionsProvider and VersionService.
    VersionService is using InstalledVersionsProvider.
    
    VersionService
    + Delete(versionname)
    + GetVersionNames e.g "1.12"
    + GetVersionDirectories e.g "C:\sth\1.12"

---
+++ SandstoneLauncher.Minecraft.Cli/Services/Version/VersionService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using SandstoneLauncher.Minecraft.Cli.Types;
+using SandstoneLauncher.Minecraft.Types;
+using Spencer.NET;
+
+namespace SandstoneLauncher.Minecraft.Cli.Services.Version
+{
+    [SingleInstance]
+    public class VersionService
+    {
+        private InstalledVersionsProvider InstalledVersionsProvider;
+        private VersionPathProvider VersionPathProvider;
+        
+        public VersionService(InstalledVersionsProvider installedVersionsProvider, VersionPathProvider versionPathProvider)
+        {
+            InstalledVersionsProvider = installedVersionsProvider;
+            VersionPathProvider = versionPathProvider;
+        }
+        
+        public IEnumerable<string> GetVersionNames()
+        {
+            return InstalledVersionsProvider.GetInstalledVersionsDirs().Select(Path.GetFileName);
+        }
+
+        public IEnumerable<string> GetVersionDirectories()
+        {
+            return InstalledVersionsProvider.GetInstalledVersionsDirs();
+        }
+
+        public Task<bool> DeleteVersion(string versionName)
+        {
+            try
+            {
+                var versionPath = VersionPathProvider.ProvideVersionPath(versionName);
+                Directory.Delete(versionPath, true);
+                return Task.FromResult(true);
+            }
+            catch (IOException)
+            {
+                return Task.FromResult(false);
+            }
+        }
+    }
+}
---
+++ SandstoneLauncher.Minecraft.Cli/Types/InstalledVersionsProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using SandstoneLauncher.Minecraft.Types;
+using Spencer.NET;
+
+namespace SandstoneLauncher.Minecraft.Cli.Types
+{
+    [SingleInstance]
+    public class InstalledVersionsProvider
+    {
+        public IEnumerable<string> GetInstalledVersionsDirs()
+        {
+            return Directory.GetDirectories(new MinecraftDirectory().GetVersions());
+        }
+    }
+}

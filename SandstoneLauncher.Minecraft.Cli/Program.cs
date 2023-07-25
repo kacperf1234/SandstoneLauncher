7417fa59775a3498fb60502644dcdd92151a651c
Kacper Faber <kacperf1234@gmail.com>
2023-07-25 11:07:14 +0200

    Created SandstoneLauncher.Minecraft.Cli
    - Added Spencer.NET and ConsoleAppFramework.

---
+++ SandstoneLauncher.Minecraft.Cli/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using ConsoleAppFramework;
+using SandstoneLauncher.Minecraft.Cli.Commands;
+using SandstoneLauncher.Minecraft.Cli.Types;
+using Spencer.NET;
+
+namespace SandstoneLauncher.Minecraft.Cli
+{
+    class Program
+    {
+        static void AddCommands(ConsoleApp app)
+        {
+            app.AddCommands<VersionCommands>();
+        }
+        
+        static async Task Main(string[] args)
+        {
+            StaticContainer.Container = ContainerFactory.Container();
+            StaticContainer.Container.RegisterAssembly<VersionCommands>();
+            
+            var app = ConsoleApp.Create(args);
+            AddCommands(app);
+
+            await app.RunAsync();
+        }
+    }
+}
